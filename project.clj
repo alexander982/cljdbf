8c329ceb28e56ac9afa3b00caaea08e0859ddb98
Alexander Kozlov <asmailiks@gmail.com>
2016-06-24 09:07:46 +0300

    Change dependency to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Чтение(экспорт в CSV) DBF-файлов"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot dbf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
