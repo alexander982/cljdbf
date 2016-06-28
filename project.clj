66e76cb77a3b8ae80d6cbe9252636654069b396f
Alexander Kozlov <asmailiks@gmail.com>
2016-06-28 13:31:56 +0300

    Update project file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dbf "0.1.1-SNAPSHOT"
+(defproject dbf "0.1.1"
   :description "Чтение(экспорт в CSV) DBF-файлов"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
